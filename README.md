66dcf1a39a08136c9b76910f40a23fc63690b2e1
Homerocker <molodoy3561@gmail.com>
2019-10-14 11:57:48 +0300

    initial commit
    - added OnTooltipSetItem hook to display item IDs on tooltips
    - display IDs in AltasLoot tooltips
    - display IDs for embedded items tooltips

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TootTipID
- 
+ Requires: [LibItemID](https://github.com/Homerocker/LibItemID)
+ Displays items, abilities, buffs, debuffs IDs on game tooltips.
---
+++ TooltipID/TooltipID.lua
@@ -0,0 +1,55 @@
+hooksecurefunc(GameTooltip, "SetUnitBuff", function(self,...)
+	local id = select(11,UnitBuff(...))
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self,...)
+	local id = select(11,UnitDebuff(...))
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+hooksecurefunc(GameTooltip, "SetUnitAura", function(self,...)
+	local id = select(11,UnitAura(...))
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+hooksecurefunc("SetItemRef", function(link, text, button, chatFrame)
+	if string.find(link,"^spell:") then
+		local id = string.sub(link,7)
+		ItemRefTooltip:AddDoubleLine("SpellID:",id)
+		ItemRefTooltip:Show()
+	end
+end)
+
+GameTooltip:HookScript("OnTooltipSetSpell", function(self)
+	local id = select(3,self:GetSpell())
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+GameTooltip:HookScript("OnTooltipSetItem", function(self)
+  local id = GetItemID(select(2, self:GetItem()))
+	if id then
+		self:AddDoubleLine("ItemID:",id)
+		self:Show()
+	end
+end)
+
+AtlasLootTooltip:HookScript("OnTooltipSetItem", function(self)
+  local id = GetItemID(select(2, self:GetItem()))
+	if id then
+		self:AddDoubleLine("ItemID:",id)
+		self:Show()
+	end
+end)
